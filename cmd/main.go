f5e058b161ff5446f761e5229d9c011e0945d9cc
MatthewWertman <matthewwertman57@gmail.com>
2025-07-31 09:37:01 -0700

    find lan interface

--- cmd/main.go
+++ cmd/main.go
@@ -8,12 +8,15 @@ import (
 )
 
 func main() {
-	iface, err := iprd.GetInterfaceByName("vtnet0")
+	iface, err := iprd.FindLANInterface()
 	if err != nil {
 		log.Panicln(err)
 	}
 	fmt.Printf("%+v\n", *iface)
 	if iface.IsUp() {
 		fmt.Println("interface is up!")
+	}
+	if iface.IsLan() {
+		fmt.Println("interface is marked LAN.")
 	}
 }
