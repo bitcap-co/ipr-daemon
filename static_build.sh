4e1c44007d7baf773638dd785e82c9d9f1373daa
MatthewWertman <matthewwertman57@gmail.com>
2025-07-31 10:21:53 -0700

    add link flag for libpcap

--- static_build.sh
+++ static_build.sh
@@ -2,4 +2,4 @@
 ##
 ## build static binary for freebsd
 
-CGO_ENABLED=1 sudo go124 build -tags netgo -ldflags "-linkmode 'external' -extldflags '-static -libverbs'" -o iprd cmd/main.go
+CGO_ENABLED=1 sudo go124 build -tags netgo -ldflags "-linkmode 'external' -extldflags '-static -libverbs -lpcap'" -o iprd cmd/main.go
