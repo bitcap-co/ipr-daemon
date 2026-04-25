1a11ca0b49cfabc7eb373f94a5c2cf33c91913ce
MatthewWertman <matthewwertman57@gmail.com>
2026-04-24 22:24:30 -0700

    Update record_test.go

--- tests/record_test.go
+++ tests/record_test.go
@@ -2,6 +2,7 @@ package iprd_test
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 	"time"
 
@@ -39,27 +40,51 @@ func TestCapacity(t *testing.T) {
 }
 
 func TestUpdateExistingEntry(t *testing.T) {
-	record := iprd.NewRecord(10)
-	// fill record with some entries
-	fillRecord(record, 4)
+	record := iprd.NewRecord(5)
+	fillRecord(record, 5)
 	len := record.Length()
-	// try and update an existing entry
-	entry := iprd.RecordEntry{
-		SrcIP:     "192.168.1.1",
+
+	// update existing entry
+	key := "test 1"
+	ent := record.Get(key)
+	test_entry := iprd.RecordEntry{
+		SrcIP:     "192.168.1.111",
 		SrcMAC:    "aa:bb:cc:dd:ee:ff",
 		MinerHint: iprd.Antminer,
 		CreatedAt: time.Now().UnixMilli(),
 	}
-	record.Add("test 4", entry)
-	got_len := record.Length()
-	if got_len != len {
-		t.Errorf("got %d, want %d", got_len, len)
+	record.Add(key, test_entry)
+	updatedEnt := record.Get(key)
+	assertLength(t, record.Length(), len)
+	if reflect.DeepEqual(ent, updatedEnt) {
+		t.Errorf("got %+v, wanted %+v", updatedEnt, test_entry)
 	}
-	got_entry := record.Get("test 4")
-	if got_entry.SrcIP != entry.SrcIP {
-		t.Errorf("got %s, want %s", got_entry.SrcIP, entry.SrcIP)
-	}
-	if got_entry.UpdatedAt == 0 {
-		t.Errorf("got %d, want not zero", got_entry.UpdatedAt)
+}
+
+func TestRemoveEntry(t *testing.T) {
+	record := iprd.NewRecord(5)
+	fillRecord(record, 5)
+	len := record.Length()
+
+	t.Run("remove non-existing entry", func(t *testing.T) {
+		err := record.Remove("doesn't exist")
+		if err == nil {
+			t.Errorf("wanted error, got nil")
+		}
+		assertLength(t, record.Length(), len)
+	})
+	t.Run("remove existing entry", func(t *testing.T) {
+		err := record.Remove("test 1")
+		if err != nil {
+			t.Errorf("got %v, wanted nil", err)
+		}
+		assertLength(t, record.Length(), 4)
+	})
+}
+
+func assertLength(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got length %d, wanted %d", got, want)
 	}
 }
