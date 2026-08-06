54f55fb6c8993e38ca33c8ed77b5cbdc186769fc
MatthewWertman <matthewwertman57@gmail.com>
2026-08-05 21:41:29 -0700

    offline: support PCAP/PCAG-NG parsing, show received iface if available

--- cmd/offline/main.go
+++ cmd/offline/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"flag"
 	"fmt"
+	"io"
+	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/bitcap-co/ipr-daemon/pkg/iprd"
 	"github.com/gopacket/gopacket"
+	"github.com/gopacket/gopacket/layers"
 	"github.com/gopacket/gopacket/pcap"
+	"github.com/gopacket/gopacket/pcapgo"
 )
 
 var (
 	// flags
-	flPcapFile = flag.String("f", "", "File descriptor for .pcap file.")
+	flPcapFile = flag.String("f", "", "Path to a .pcap or .pcapng capture file.")
 	flDebug    = flag.Bool("d", false, "Switch to enable packet debugging output to stdout.")
 
 	log = iprd.NewLogger()
@@ -46,36 +51,101 @@ func main() {
 }
 
 func dumpPcap(fd string, debug bool) error {
+	file, err := os.Open(fd)
+	if err != nil {
+		return err
+	}
+	var magic [4]byte
+	if _, err := io.ReadFull(file, magic[:]); err != nil {
+		file.Close()
+		return err
+	}
+	if _, err := file.Seek(0, io.SeekStart); err != nil {
+		file.Close()
+		return err
+	}
+	if bytes.Equal(magic[:], []byte{0x0a, 0x0d, 0x0d, 0x0a}) {
+		defer file.Close()
+		return dumpPcapNG(file, debug)
+	}
+	file.Close()
+	return dumpClassicPcap(fd, debug)
+}
+
+func dumpClassicPcap(fd string, debug bool) error {
 	handle, err := pcap.OpenOffline(fd)
 	if err != nil {
 		return err
 	}
-	var packetCount int64 = 0
+	defer handle.Close()
+
 	processor := iprd.NewPacketProcessor(nil)
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	var packetCount int64
 	for packet := range packetSource.Packets() {
 		packetCount++
-		if debug {
-			log.Debug("--- Dumped Packet ---")
-			log.Debug(fmt.Sprintf("%s\n", packet.Dump()))
-		}
-		ipr, err := iprd.NewIPReportPacket(packet)
-		if err != nil {
-			log.Error(fmt.Errorf("failed to decode packet %d: %s", packetCount, err))
-			continue
-		}
-		if err := processor.ParseIPReportPacket(ipr); err != nil {
-			if errors.Is(err, iprd.ErrDuplicatePacket) {
-				// ignore duplicate packets
-				if debug {
-					log.Warn(fmt.Sprintf("cnt:%d %s - Duplicate", packetCount, ipr.String()))
-				}
-				continue
-			}
-			log.Error(fmt.Errorf("cnt:%d %s - Not valid: %w", packetCount, ipr.String(), err))
-			continue
-		}
-		log.Info(fmt.Sprintf("cnt:%d %s - Valid IP report", packetCount, ipr.String()))
+		processOfflinePacket(packet, processor, packetCount, "classic", debug)
 	}
 	return nil
+}
+
+func dumpPcapNG(file *os.File, debug bool) error {
+	reader, err := pcapgo.NewNgReader(file, pcapgo.NgReaderOptions{WantMixedLinkType: true})
+	if err != nil {
+		return err
+	}
+	processor := iprd.NewPacketProcessor(nil)
+	var packetCount int64
+	for {
+		data, ci, err := reader.ReadPacketData()
+		if errors.Is(err, io.EOF) {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		packetCount++
+
+		intf, err := reader.Interface(ci.InterfaceIndex)
+		if err != nil {
+			return err
+		}
+		interfaceName := intf.Name
+		if interfaceName == "" {
+			interfaceName = fmt.Sprintf("#%d", ci.InterfaceIndex)
+		}
+		linkType := intf.LinkType
+		if len(ci.AncillaryData) > 0 {
+			if capturedLinkType, ok := ci.AncillaryData[0].(layers.LinkType); ok {
+				linkType = capturedLinkType
+			}
+		}
+		packet := gopacket.NewPacket(data, linkType, gopacket.Default)
+		packet.Metadata().CaptureInfo = ci
+		processOfflinePacket(packet, processor, packetCount, interfaceName, debug)
+	}
+}
+
+func processOfflinePacket(packet gopacket.Packet, processor *iprd.PacketProcessor, packetCount int64, interfaceName string, debug bool) {
+	prefix := fmt.Sprintf("cnt:%d iface:%s", packetCount, interfaceName)
+	if debug {
+		log.Debug("--- Dumped Packet ---")
+		log.Debug(fmt.Sprintf("%s\n", packet.Dump()))
+	}
+	ipr, err := iprd.NewIPReportPacket(packet)
+	if err != nil {
+		log.Error(fmt.Errorf("%s - failed to decode packet: %w", prefix, err))
+		return
+	}
+	if err := processor.ParseIPReportPacket(ipr); err != nil {
+		if errors.Is(err, iprd.ErrDuplicatePacket) {
+			if debug {
+				log.Warn(fmt.Sprintf("%s %s - Duplicate", prefix, ipr.String()))
+			}
+			return
+		}
+		log.Error(fmt.Errorf("%s %s - Not valid: %w", prefix, ipr.String(), err))
+		return
+	}
+	log.Info(fmt.Sprintf("%s %s - Valid IP report", prefix, ipr.String()))
 }
---
+++ cmd/offline/main_test.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bytes"
+	"net"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/bitcap-co/ipr-daemon/pkg/iprd"
+	"github.com/gopacket/gopacket"
+	"github.com/gopacket/gopacket/layers"
+	"github.com/gopacket/gopacket/pcapgo"
+)
+
+func offlineTestPacket(t *testing.T) ([]byte, gopacket.CaptureInfo) {
+	t.Helper()
+	eth := &layers.Ethernet{
+		SrcMAC:       net.HardwareAddr{0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0x21},
+		DstMAC:       net.HardwareAddr{0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+		EthernetType: layers.EthernetTypeIPv4,
+	}
+	ip := &layers.IPv4{
+		Version:  4,
+		TTL:      64,
+		Protocol: layers.IPProtocolUDP,
+		SrcIP:    net.ParseIP("192.168.1.100").To4(),
+		DstIP:    net.ParseIP("255.255.255.255").To4(),
+	}
+	udp := &layers.UDP{SrcPort: 5000, DstPort: 14235}
+	if err := udp.SetNetworkLayerForChecksum(ip); err != nil {
+		t.Fatal(err)
+	}
+	buf := gopacket.NewSerializeBuffer()
+	if err := gopacket.SerializeLayers(buf, gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}, eth, ip, udp, gopacket.Payload("IP report from 192.168.1.100")); err != nil {
+		t.Fatal(err)
+	}
+	data := append([]byte(nil), buf.Bytes()...)
+	return data, gopacket.CaptureInfo{
+		Timestamp:     time.Now(),
+		CaptureLength: len(data),
+		Length:        len(data),
+	}
+}
+
+func TestDumpPcapNGIncludesInterfaceName(t *testing.T) {
+	path := filepath.Join(t.TempDir(), "capture.pcapng")
+	data, ci := offlineTestPacket(t)
+	writer := iprd.NewCaptureWriter(path, false, iprd.NewLogger())
+	if err := writer.Open(); err != nil {
+		t.Fatal(err)
+	}
+	if err := writer.Write(iprd.CapturedPacket{
+		Data:        data,
+		CaptureInfo: ci,
+		LinkType:    layers.LinkTypeEthernet,
+		Interface:   iprd.IPRInterface{Index: 3, Name: "test0"},
+	}); err != nil {
+		t.Fatal(err)
+	}
+	if err := writer.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	var output bytes.Buffer
+	log.SetOutput(&output)
+	defer log.SetOutput(os.Stdout)
+	if err := dumpPcap(path, false); err != nil {
+		t.Fatal(err)
+	}
+	if !strings.Contains(output.String(), "iface:test0") {
+		t.Fatalf("output does not identify PCAP-NG interface:\n%s", output.String())
+	}
+}
+
+func TestDumpClassicPcapRemainsSupported(t *testing.T) {
+	path := filepath.Join(t.TempDir(), "capture.pcap")
+	data, ci := offlineTestPacket(t)
+	file, err := os.Create(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	writer := pcapgo.NewWriter(file)
+	if err := writer.WriteFileHeader(1600, layers.LinkTypeEthernet); err != nil {
+		t.Fatal(err)
+	}
+	if err := writer.WritePacket(ci, data); err != nil {
+		t.Fatal(err)
+	}
+	if err := file.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	var output bytes.Buffer
+	log.SetOutput(&output)
+	defer log.SetOutput(os.Stdout)
+	if err := dumpPcap(path, false); err != nil {
+		t.Fatal(err)
+	}
+	if !strings.Contains(output.String(), "iface:classic") {
+		t.Fatalf("output does not identify classic PCAP input:\n%s", output.String())
+	}
+}
